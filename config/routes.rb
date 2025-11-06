54a5f65dc93cf45537c27a0ee5c30c1b95be2f4b
thibault dupuis <dupuis.thib81@gmail.com>
2025-11-06 21:41:58 +0100

    watch-list part 1

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   #root to: "lists#index"
+   get "movies", to: "movies#index"
+   get "lists", to: "lists#index"
 end
