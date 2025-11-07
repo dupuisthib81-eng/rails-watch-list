51e38ff69ec424c9d2da21ae80dbd6aeeca67ad6
thibault dupuis <dupuis.thib81@gmail.com>
2025-11-07 22:21:58 +0100

    what watch

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    #root to: "lists#index"
-   get "movies", to: "movies#index"
-   get "lists", to: "lists#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
 end
